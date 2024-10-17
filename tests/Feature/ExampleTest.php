94ca3a3f80e041a9a158d7877885a487da9785e8
shukrirauf@gmail.com <shukrirauf@gmail.com>
2024-10-17 22:52:52 +0530

    creating tests for creating posts and admin approve status and home page loading

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,16 +2,24 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Post;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        $response = $this->post('/posts', [
+            'title' => 'New Test Post',
+            'content' => 'This is the content for the test post.',
+        ]);
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
